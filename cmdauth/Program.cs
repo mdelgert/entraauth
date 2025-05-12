e0b49e8682eba69aeb385a28163f63ececb23017
Matthew David Elgert <mdelgert@yahoo.com>
2025-05-12 16:44:52 -0400

    Add Microsoft Graph SDK and implement client credentials flow in Program.cs

--- cmdauth/Program.cs
+++ cmdauth/Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Azure.Identity;
+using System.Linq;
+using Microsoft.Graph;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -36,6 +39,40 @@ var env = configuration.GetValue<string>("Environment");
 
 // Log the environment
 logger.LogInformation($"Environment: {env}");
+
+
+//https://learn.microsoft.com/en-us/graph/sdks/choose-authentication-providers?tabs=csharp
+
+// The client credentials flow requires that you request the
+// /.default scope, and pre-configure your permissions on the
+// app registration in Azure. An administrator must grant consent
+// to those permissions beforehand.
+var scopes = new[] { "https://graph.microsoft.com/.default" };
+
+// Values from app registration
+var clientId = configuration.GetValue<string>("AzureAd:ClientId");
+var tenantId = configuration.GetValue<string>("AzureAd:TenantId");
+
+// Retrieve the ClientSecret from the ClientCredentials array
+var clientSecret = configuration.GetSection("AzureAd:ClientCredentials")
+    .GetChildren()
+    .FirstOrDefault(c => c.GetValue<string>("SourceType") == "ClientSecret")
+    ?.GetValue<string>("ClientSecret");
+
+// Log the clientId, tenantId, and clientSecret
+logger.LogInformation($"ClientId: {clientId} TenantId: {tenantId} ClientSecret: {clientSecret}");
+
+// using Azure.Identity;
+var options = new ClientSecretCredentialOptions
+{
+    AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
+};
+
+// https://learn.microsoft.com/dotnet/api/azure.identity.clientsecretcredential
+var clientSecretCredential = new ClientSecretCredential(
+    tenantId, clientId, clientSecret, options);
+
+var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
 
 logger.LogInformation("End:");
 
