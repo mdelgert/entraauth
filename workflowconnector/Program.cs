d3e1b0f6049b561ab86d83b72dfd6c7eb04803d2
Matthew David Elgert <mdelgert@yahoo.com>
2025-05-13 19:11:36 -0400

    Add workflowconnector project with WeatherForecast API and Docker support

---
+++ workflowconnector/Program.cs
@@ -0,0 +1,36 @@
+using AspNetCore.Swagger.Themes;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI(ModernStyle.Dark);
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/")
+    {
+        context.Response.Redirect("/swagger");
+        return;
+    }
+    await next();
+});
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
---
+++ workflowconnector/README.md
@@ -0,0 +1,4 @@
+# Links
+
+https://github.com/Azure-Samples/active-directory-dotnet-external-identities-api-connector-azure-function-validate/blob/master/SignUpValidation.cs
+
