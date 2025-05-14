c11cd145dafa0b911c69296514896d7d471e44e6
Matthew David Elgert <mdelgert@yahoo.com>
2025-05-13 20:25:29 -0400

    Implement logging in LoginController for login requests

--- workflowconnector/Controllers/LoginController.cs
+++ workflowconnector/Controllers/LoginController.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿//https://github.com/Azure-Samples/active-directory-dotnet-external-identities-api-connector-azure-function-validate/blob/master/SignUpValidation.cs
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using workflowconnector.Services;
 
 namespace workflowconnector.Controllers
 {
-    public class LoginController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly DbService _context;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public LoginController(DbService context, ILogger<WeatherForecastController> logger)
         {
-            return View();
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetLogin")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                _logger.LogInformation("Login requested");
+
+                var log = new Models.LogModel
+                {
+                    LogLevel = "Info",
+                    Message = "Login requested"
+                };
+
+                await _context.Logs.AddAsync(log);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while logging the login request");
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing your request.");
+            }
+
+            return Ok("Hello World!");
         }
     }
 }
